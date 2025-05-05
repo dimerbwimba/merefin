4dd7faef0bf598b58f106905532e8f420089271e
dimerbwimba <dimerbwimba191996@gmail.com>
2025-05-05 11:33:09 +0200

    fix the bycript issues

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,7 +2,7 @@ import type { NextAuthOptions } from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import { db } from "@/lib/db"
-import { compare } from "bcrypt"
+import { compare } from "bcryptjs"
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(db),
